929fe2a8dd6ac831301f2dbe62547d0c3023c511
NauGaika <cklyarenkovlad@gmail.com>
2019-01-22 01:57:46 +0300

    исправления с меню и ip

--- store/CommonProps.js
+++ store/CommonProps.js
@@ -14,7 +14,7 @@ const CommonProps = {
   actions: {
     getCommonProps ({ commit, state }) {
       return new Promise((resolve, reject) => {
-        const path = `http://127.0.0.1:5000/api/get-all/common_props`
+        const path = `http:/api.sev-steklo.ru/api/get-all/common_props`
         axios.get(path)
         .then(response => {
           let data = response.data
