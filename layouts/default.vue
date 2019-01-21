c4a883f8feb2af15d43e87191cd5d0982a12a542
NauGaika <cklyarenkovlad@gmail.com>
2019-01-22 02:37:04 +0300

    исправления с меню и ip

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,7 +19,7 @@ import FooterContainer from '~/components/Footer/FooterContainer.vue'
       'header-container': HeaderContainer,
       'footer-container': FooterContainer
     },
-    mounted: {
+    mounted () {
       if (process.browser) {
          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}
          m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
